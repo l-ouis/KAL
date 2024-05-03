6093a0e1d4c17202c91c6b6acb85303272bea013
kyleklee <90531161+kyleklee@users.noreply.github.com>
2024-05-02 17:08:54 -0700

    Update length_normalize.py

--- src/preprocessing/length_normalize.py
+++ src/preprocessing/length_normalize.py
@@ -11,10 +11,11 @@ with open('label_tokens.pkl', 'rb') as f:
 with open('tokenizer.pkl', 'rb') as f:
     tokenizer = pickle.load(f)
 
+tokenizer.vocab['Start'] = 258
 print("Length of pickled input and label lists:")
 print(len(input_tokens_list))
 print(len(label_tokens_list))
-
+print(tokenizer.vocab)
 final_input_ids = []
 final_label_ids = []
 
@@ -24,14 +25,16 @@ for (input, label) in zip(input_tokens_list, label_tokens_list):
     input_tokens = input[0]
     label_tokens = label[0]
     input_ids = input_tokens.ids
+    input_ids = [input_ids[i] for i in range(len(input_ids)-1) if input_ids[i] != 4 or input_ids[i] != input_ids[i+1]]
     label_ids = label_tokens.ids
+    label_ids = [label_ids[i] for i in range(len(label_ids)-1) if label_ids[i] != 4 or label_ids[i] != label_ids[i+1]]
 
     min_length = min(len(input_ids), len(label_ids))
     batches = min_length // 256
     if min_length > 256:
         # Split the input and label ids into batches of 256 until they can't be split anymore
-        input_batches = [input_ids[i*256:(i+1)*256] for i in range(batches)]
-        label_batches = [label_ids[i*256:(i+1)*256] for i in range(batches)]
+        input_batches = [258] + [input_ids[i*256:(i+1)*256] for i in range(batches)]
+        label_batches = [258] + [label_ids[i*256:(i+1)*256] for i in range(batches)]
 
         # Each element in these batches will be a single data point.
         # Append these to the final lists
